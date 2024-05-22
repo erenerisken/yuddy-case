0e9b168f74f59f47c42880a2159a7a33d962f0cb
erenerisken <erenerisken@gmail.com>
2024-05-22 13:10:29 +0300

    Implement product interface and add fixtures

--- src/interfaces/Product.ts
+++ src/interfaces/Product.ts
@@ -1,3 +1,17 @@
 export interface Product {
   id: number;
+  name: string;
+  images: string[];
+  basePrice: number;
+  discountedPrice?: number;
+  discountRatio?: number;
+  summary: string;
+  description: string;
+  stock: number;
+  brandID: number;
+  isNewProduct: boolean;
+  isOnSale: boolean;
+  specification: {
+    [key: string]: string;
+  };
 }
